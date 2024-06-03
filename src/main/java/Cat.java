4d669f901f8ecebd16572e4b4b85162486e8e61d
DmitryKtitorov <dimaktitorov1@gmail.com>
2024-06-03 16:27:41 +0300

    1st

---
+++ src/main/java/Cat.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
